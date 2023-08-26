d3cd5844fb0947f466b8c969b27dbd1c1b5877a6
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-08-26 18:02:11 -0500

    foreach

---
+++ src/SentenciaForEach.java
@@ -0,0 +1,10 @@
+public class SentenciaForEach {
+    public static void main(String[] args) {
+
+        int[] numeros = {1,3,5,7,9,11,13,15 };
+        for (int num:numeros) {
+            System.out.println("num = " + num);
+        }
+
+    }
+}
