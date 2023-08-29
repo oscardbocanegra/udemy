83aca4f19564ba0b338dd2c2c51aa220269ad407
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-08-28 20:58:27 -0500

    completando ejercicio

--- cursoPracticoDjango/webpersonal/core/views.py
+++ cursoPracticoDjango/webpersonal/core/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render, HttpResponse
 
+html_base = """
+<h1>Mi web personal</h1>
+<ul>
+    <li><a href="/"> Portada </a></li>
+    <li><a href="/about-me/"> Acerca de </a></li>
+    <li><a href="/portfolio/"> Portafolio </a></li>
+    <li><a href="/contact/"> Contacto </a></li>
+</ul>
+"""
+
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1> Mi web personal </h1><h2> Portada </h2>")
+    return HttpResponse(html_base + """ 
+                        <h2>Portada</h2>
+                        <p>Esto es la portada</p>
+                    """)
+
+def about(request):
+    return HttpResponse(html_base + """
+                        <h2>Acerca de</h2>
+                        <p>Me llamo Oscar y me encanta la programacion :)</p>
+                    """)
+    
+def portfolio (request):
+    return HttpResponse(html_base + """
+                        <h2> Portafolio </h2>
+                        <p> Aca se va a encontrar el portafolio </p>
+                    """)
+    
+def contact (request):
+    return HttpResponse(html_base + """
+                        <h2> Contacto </h2>
+                        <p> Aca se encontrata todo hacerca del contacto </p>                    
+                    """)
