7314520e2dc1e13650bee75068699ce099099260
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-09-19 22:26:47 -0500

    building templates

--- django_master/food/views.py
+++ django_master/food/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
+from django.template import loader
 
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
-    return HttpResponse(item_list)
+    template = loader.get_template('food/index.html')
+    contex = {
+        
+    }
+    return HttpResponse(template.render(contex,request))
 
 def item(request):
     return HttpResponse('<h1>This is an item view</h1>')
