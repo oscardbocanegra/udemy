bc052d347946b300bee557a2a99f65b64b5e8e75
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-11-14 20:39:50 -0500

    starting a new course about Django

--- django_master/food/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('item/', views.item, name= 'item'),
-    path('<int:item_id>/', views.detail, name='detail' )
-]
--- django_masterclass/mysite/mysite/settings.py
+++ django_masterclass/mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iy0_(^t^3s1_y*c)obi!@#%a@(cz*0^wu_(+!e8@2u&ws))03k'
+SECRET_KEY = 'django-insecure-b$xg810z@llg1lhvy#+sxa!sq%#@x^0e^2#a@7ob+)@3t9nsp)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'food.apps.FoodConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
