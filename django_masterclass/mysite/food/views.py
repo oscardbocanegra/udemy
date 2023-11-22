2d33c29677f8b4483dc43ddf742ecca62fb0ef0d
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-11-21 22:25:36 -0500

    passing context

--- django_masterclass/mysite/food/views.py
+++ django_masterclass/mysite/food/views.py
@@ -6,11 +6,10 @@ from django.template import loader
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
-    template = loader.get_template('food/index.html')
     context = {
-        
+        'item_list' : item_list,
     }
-    return HttpResponse(template.render(context,request))
+    return render(request, 'food/index.html', context)
 
 def item(request):
     return HttpResponse('<h1>This is an item view</h1>')
