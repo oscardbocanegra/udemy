22217a9e734ca22b764fe3e2824d05825c6f425c
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-11-28 21:49:26 -0500

    second project

---
+++ python_total/projects/second_project.py
@@ -0,0 +1,6 @@
+#Los vendedores reciben comiciones del 13% por sus ventas totales, calcular las comiciones preguntando el nombre y cunto han vendido 
+
+nombre = input("Cual es tu nombre? ")
+ventas = float(input("Cuanto has vendido en el ultimo mes? "))
+porcentaje = ventas * 13 / 100
+total = print(f"ok {nombre}, este mes ganaste {porcentaje}")
