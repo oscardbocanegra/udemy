d180c9d0d8c14c3f08750cc2f418dc8e934589cc
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-12-09 17:51:14 -0500

    practicando con return

---
+++ python_total/projects/funciones.py
@@ -0,0 +1,10 @@
+#Práctica Return 1: Crea una función llamada potencia 
+#que tome dos valores numéricos como argumentos. Deberá 
+#devolver el número que resulte de resolver una potencia, 
+#utilizando el primer número como base, y el segundo como exponente
+
+def potencia(numero1, numero2):
+    return numero1**numero2
+    
+result = potencia(3,4)
+print(result)
