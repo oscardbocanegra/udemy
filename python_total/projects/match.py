a4510632b6b9f94cdf3d2f6911a33a548111be15
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-12-09 14:27:29 -0500

    learning about match

---
+++ python_total/projects/match.py
@@ -0,0 +1,11 @@
+serie = "N-02"
+
+match serie:
+    case "N-01":
+        print("Samsung")
+    case "N-02":
+        print("Nokia")
+    case "N-03":
+        print("Motorola")
+    case _:
+        print("No existe este producto")
