54929be92168087c5aff2969ed4a812535c467fa
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-12-11 20:13:02 -0500

    more about functions

---
+++ python_total/projects/interaccion_funciones.py
@@ -0,0 +1,34 @@
+from random import shuffle
+
+#Lista inicial
+palitos = ['-', '--', '---', '----']
+
+#Mezclar palitos
+
+def mezclar(lista):
+    shuffle(lista)
+    return lista
+
+#Pedirle intento 
+def probar_suerte():
+    intento = ''
+    
+    while intento not in ['1', '2', '3', '4']:
+        intento = input("Elige un numero del 1 al 4: ")
+    
+    return int(intento)
+
+def chequear_intento(lista, intento):
+    if lista[intento -1] == '-':
+        print("Has perdido")
+    
+    else:
+        print("Te has salvado")
+    
+    print(f"Te ha tocado {lista[intento-1]}")
+    
+palitos_mezclados= mezclar(palitos)
+
+seleccion = probar_suerte()
+
+chequear_intento(palitos_mezclados, seleccion)
