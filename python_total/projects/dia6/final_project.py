5fa136948a8653dae0f9491371620a95c64e99ac
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-12-17 16:13:35 -0500

    building the menu in a function

--- python_total/projects/dia6/final_project.py
+++ python_total/projects/dia6/final_project.py
@@ -10,6 +10,33 @@ def contar_recetas(ruta):
         contador += 1
     return contador
 
+def inicio():
+    system('clear')
+    print("*" * 50)
+    print('*' * 5 ," Bienvenido al administrador de recetas ", '*' * 5)
+    print("*" * 50)
+    print("\n")
+    print(f"Las recetas se encuentran en: {mi_ruta}")
+    print(f"Total recetas: {contar_recetas(mi_ruta)}")
+    
+    
+    eleccion_menu = "x"
+    while not eleccion_menu.isnumeric() or int(eleccion_menu) not in range(1,7):
+        print("Elige una opcion")
+        print("""
+              [1] - Leer receta
+              [2] - Crear receta nueva
+              [3] - Crear categoria nueva
+              [4] - Eliminar receta
+              [5] - Eliminar categoria
+              [6] - Salir del programa""")
+        eleccion_menu = input()
+    return (eleccion_menu)
+    
+inicio()
+
+
+
 menu = 0
 
 if menu == 1:
