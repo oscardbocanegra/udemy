0fe40c315266c4dc94f1abbc6af24aa4e93ef47f
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-12-24 18:48:16 -0500

    ejercicios 6

--- python_total/projects/dia7/ejercicios6.py
+++ python_total/projects/dia7/ejercicios6.py
@@ -53,3 +53,25 @@ class Mamifero(Vertebrado):
 
 class Ornitorrinco(Mamifero, Pez, Reptil, Ave):
     pass
+
+#Práctica Herencia Extendida 3: Un hijo ha heredado de su padre todas sus características, 
+#sin embargo, tienen diferentes hobbies. Logra que la clase Hijo herede de Padre todos sus 
+#métodos y atributos, sobreescribiendo el método hobby() para que se devuelva[1]: "Juego 
+#videojuegos en mi tiempo libre" [1]: asegúrate de utilizar return seguido de una cadena de texto
+class Padre():
+    color_ojos = "marrón"
+    tipo_pelo = "rulos"
+    altura = "media"
+    voz = "grave"
+    deporte_preferido = "tenis"
+    def reir(self):
+        return "Jajaja"
+    def hobby(self):
+        return "Pinto madera en mi tiempo libre"
+    def caminar(self):
+        return "Caminando con pasos largos y rápidos"
+        
+class Hijo(Padre):
+    
+    def hobby(self):
+        return "Juego videojuegos en mi tiempo libre"
