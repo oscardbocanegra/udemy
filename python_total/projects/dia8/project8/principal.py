2dfd63a4b53011f8be239fcdf4fb30d8983149d8
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-12-28 18:53:27 -0500

    finish project 8

---
+++ python_total/projects/dia8/project8/principal.py
@@ -0,0 +1,34 @@
+import project
+
+def preguntar():
+    print("Bienvenido a farmacia Python")
+    
+    while True:
+        print("[P] - Perfumeria\n[F] - Farmacia \n[C] - Cosmetica")
+        try:
+            mi_rubro = input("Elija su rubro: ").upper()
+            ['P', 'F', 'C'].index(mi_rubro)
+        except ValueError:
+            print("Esa no es una opcion valida")
+            
+        else:
+            break
+        
+    project.decorador(mi_rubro)
+    
+def inicio():
+    
+    while True:
+        preguntar()
+        try:
+            otro_turno = input("Quieres sacar otro turno? [S] [N]: ").upper()
+            ['S', 'N'].index(otro_turno)
+        except ValueError:
+            print("Esa no es una opcion valida")
+        else:
+            if otro_turno == 'N':
+                print("Gracias por su visita")
+                break
+            
+inicio()
+            
