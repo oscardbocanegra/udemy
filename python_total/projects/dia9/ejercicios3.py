09e2929cc8fc49b8b1f6b5e2d6565358c300da62
Oscar Bocanegra <davidbocanegrac@gmail.com>
2024-01-01 18:15:31 -0500

    ejercicios 3 con math

---
+++ python_total/projects/dia9/ejercicios3.py
@@ -0,0 +1,16 @@
+#Práctica Módulo Math 1: Obtén el logaritmo base 10 del número 25,
+#y almacena el resultado en la variable resultado.
+import math
+
+resultado = math.log10(25)
+
+#Práctica Módulo Math 2: Obten la raíz cuadrada de pi con la constante
+#math.pi y el método math.sqrt() . Almacena el resultado obtenido en la
+#variable resultado.
+
+resultado = math.sqrt(math.pi)
+
+#Práctica Módulo Math 3: Encuentra el factorial de 7 y almacena el resultado
+#en la variable resultado.El método a utilizar es factorial()
+
+resultado = math.factorial(7)
