862c2d389a4b493445686d363d6f7156278f2246
Oscar Bocanegra <davidbocanegrac@gmail.com>
2024-01-03 20:11:10 -0500

    Agregar al protagonista

--- python_total/projects/dia10/main.py
+++ python_total/projects/dia10/main.py
@@ -11,14 +11,27 @@ pygame.display.set_caption("Invacion espacial")
 icono = pygame.image.load("astronauta.png")
 pygame.display.set_icon(icono)
 
+#Jugador
+img_jugador = pygame.image.load("nave-espacial.png")
+jugador_x = 368
+jugador_y = 536
+
+def jugador():
+    pantalla.blit(img_jugador, (jugador_x, jugador_y))
+
 
 #Loop del juego
 se_ejecuta = True
 while se_ejecuta:
+    
+    #RGB
+    pantalla.fill((110, 213, 239))
+    
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             se_ejecuta = False
-            
-    pantalla.fill((110, 213, 239))
+    
+    jugador()
+    
     pygame.display.update()
             
