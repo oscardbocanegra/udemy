6b7e595ddaa6267a492b2dae882892d578852599
Oscar Bocanegra <davidbocanegrac@gmail.com>
2024-01-05 20:22:24 -0500

    modificando limites

--- python_total/projects/dia10/main.py
+++ python_total/projects/dia10/main.py
@@ -28,22 +28,36 @@ while se_ejecuta:
     #RGB
     pantalla.fill((110, 213, 239))
     
+    #iterar evento
     for evento in pygame.event.get():
+        
+        #evento cerrar
         if evento.type == pygame.QUIT:
             se_ejecuta = False
             
+        #Evento presionar flechas
         if evento.type == pygame.KEYDOWN:
             if evento.key == pygame.K_LEFT:
                 jugador_x_cambio = -0.2
             elif evento.key == pygame.K_RIGHT:
                 jugador_x_cambio = 0.2
                 
+        #Evento soltar flechas
         if evento.type == pygame.KEYUP:
             if evento.key == pygame.K_LEFT or evento.key == pygame.K_RIGHT:
                 jugador_x_cambio = 0
                 
+                
+    # Modificar ubicacion
     jugador_x += jugador_x_cambio
     jugador(jugador_x, jugador_y)
     
+    #Mantener dentro de bordes
+    if jugador_x <= 0:
+        jugador_x = 0
+    elif jugador_x >= 736:
+        jugador_x = 736
+    
+    #Actualizar
     pygame.display.update()
             
