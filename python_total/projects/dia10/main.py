58bcdd3c7c743f60186239dec5f5eb713986a9a8
Oscar Bocanegra <davidbocanegrac@gmail.com>
2024-01-05 20:55:44 -0500

    cargando imagen de fondo

--- python_total/projects/dia10/main.py
+++ python_total/projects/dia10/main.py
@@ -11,18 +11,20 @@ pantalla = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Invacion espacial")
 icono = pygame.image.load("astronauta.png")
 pygame.display.set_icon(icono)
+fondo = pygame.image.load('cielo.jpg')
 
 #Jugador
 img_jugador = pygame.image.load("nave-espacial.png")
 jugador_x = 368
-jugador_y = 536
+jugador_y = 500
 jugador_x_cambio = 0
 
 #variables del enemigo
 img_enemigo = pygame.image.load("astronave.png")
 enemigo_x = random.randint(0, 736)
 enemigo_y = random.randint(50, 200)
-enemigo_x_cambio = 0
+enemigo_x_cambio = 0.2
+enemigo_y_cambio = 50
 
 #funcion jugador
 def jugador(x, y):
@@ -37,8 +39,8 @@ def enemigo(x, y):
 se_ejecuta = True
 while se_ejecuta:
     
-    #RGB
-    pantalla.fill((110, 213, 239))
+    #imagen de fondo
+    pantalla.blit(fondo, (0,0))
     
     #iterar evento
     for evento in pygame.event.get():
@@ -50,9 +52,9 @@ while se_ejecuta:
         #Evento presionar flechas
         if evento.type == pygame.KEYDOWN:
             if evento.key == pygame.K_LEFT:
-                jugador_x_cambio = -0.2
+                jugador_x_cambio = -0.3
             elif evento.key == pygame.K_RIGHT:
-                jugador_x_cambio = 0.2
+                jugador_x_cambio = 0.3
                 
         #Evento soltar flechas
         if evento.type == pygame.KEYUP:
@@ -60,14 +62,26 @@ while se_ejecuta:
                 jugador_x_cambio = 0
                 
                 
-    # Modificar ubicacion
+    # Modificar ubicacion del jugador
     jugador_x += jugador_x_cambio
     
-    #Mantener dentro de bordes
+    #Mantener dentro de bordes del jugador
     if jugador_x <= 0:
         jugador_x = 0
     elif jugador_x >= 736:
         jugador_x = 736
+        
+    # Modificar ubicacion del enemigo
+    enemigo_x += enemigo_x_cambio
+    
+    #Mantener dentro de bordes del enemigo
+    if enemigo_x <= 0:
+        enemigo_x_cambio = 0.3
+        enemigo_y += enemigo_y_cambio
+        
+    elif enemigo_x >= 736:
+        enemigo_x_cambio = -0.3
+        enemigo_y += enemigo_y_cambio
         
     jugador(jugador_x, jugador_y)
     enemigo(enemigo_x, enemigo_y)
