92b5b3dc6d611866d8a4990d393537b41f2338d2
Oscar Bocanegra <davidbocanegrac@gmail.com>
2024-01-09 18:25:01 -0500

    extraer elementos de una clase

--- python_total/projects/dia11/web_scraping.py
+++ python_total/projects/dia11/web_scraping.py
@@ -5,5 +5,11 @@ resultado = requests.get('https://escueladirecta-blog.blogspot.com/')
 
 sopa = bs4.BeautifulSoup(resultado.text, "lxml")
 
-parrafo_especial = sopa.select('title')
-print(parrafo_especial)
+columna_lateral = sopa.select('.content')
+
+if columna_lateral:
+    # Process the elements found
+    for element in columna_lateral:
+        print(element)
+else:
+    print("No elements with class 'content' found on the page.")
