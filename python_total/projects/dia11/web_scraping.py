827b7d954ba085fdd9ab3a2e16e75def1b6888f8
Oscar Bocanegra <davidbocanegrac@gmail.com>
2024-01-09 18:08:43 -0500

    extraer titulo de la pagina

---
+++ python_total/projects/dia11/web_scraping.py
@@ -0,0 +1,9 @@
+import bs4
+import requests
+
+resultado = requests.get('https://escueladirecta-blog.blogspot.com/')
+
+sopa = bs4.BeautifulSoup(resultado.text, "lxml")
+
+parrafo_especial = sopa.select('title')
+print(parrafo_especial)
