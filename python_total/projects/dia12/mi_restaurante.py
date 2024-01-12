0636300339bf63c391abd8187686547e09c81395
Oscar Bocanegra <davidbocanegrac@gmail.com>
2024-01-11 20:15:24 -0500

    starting project day 12

---
+++ python_total/projects/dia12/mi_restaurante.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+#iniciar tkinter
+aplicacion = Tk()
+
+#Tamaño de la ventana
+aplicacion.geometry('1020x630+0+0')
+
+#Evitar maximizar
+aplicacion.title("Mi restaurante - Sistema de facturacion")
+
+#color de fondo de la ventana 
+aplicacion.config(bg='burlywood')
+
+#Evitar que la aplicacion se cierre
+aplicacion.mainloop()
