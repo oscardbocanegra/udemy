2650a667933312c69a0588dea9ba6538b8ef5120
Oscar Bocanegra <davidbocanegrac@gmail.com>
2024-01-14 16:52:22 -0500

    checkbuttons

--- python_total/projects/dia12/mi_restaurante.py
+++ python_total/projects/dia12/mi_restaurante.py
@@ -68,5 +68,46 @@ panel_recibo.pack()
 panel_botones = Frame(panel_derecha, bd=1, relief=FLAT, bg='burlywood')
 panel_botones.pack()
 
+#lista de productos
+lista_comidas = ['pollo', 'carne', 'pescado', 'pizza', 'hamburguesa', 'salchipapa', 'arroz', 'picada']
+lista_bebidas = ['agua', 'soda', 'jugo', 'cocacola', 'vino1', 'vino2', 'cerveza1', 'cerveza2']
+lista_postres = ['helado', 'fruta', 'brownies', 'flan', 'mousse', 'pastel1', 'pastel2', 'pastel3']
+
+#generar items comida 
+variables_comida = []
+contador = 0
+for comida in lista_comidas:
+    variables_comida.append('')
+    variables_comida[contador] = IntVar()
+    comida = Checkbutton(panel_comidas, text=comida.title(), font=('Dosis', 19, 'bold'),
+                         onvalue=1, offvalue=0, variable=variables_comida[contador])
+    
+    comida.grid(row=contador, column=0, sticky=W)
+    contador += 1
+    
+#generar items bebida 
+variables_bebida = []
+contador = 0
+for bebida in lista_bebidas:
+    variables_bebida.append('')
+    variables_bebida[contador] = IntVar()
+    bebida = Checkbutton(panel_bebidas, text=bebida.title(), font=('Dosis', 19, 'bold'),
+                         onvalue=1, offvalue=0, variable=variables_bebida[contador])
+    
+    bebida.grid(row=contador, column=0, sticky=W)
+    contador += 1
+    
+#generar items postres 
+variables_postres = []
+contador = 0
+for postres in lista_postres:
+    variables_postres.append('')
+    variables_postres[contador] = IntVar()
+    postres = Checkbutton(panel_postres, text=postres.title(), font=('Dosis', 19, 'bold'),
+                         onvalue=1, offvalue=0, variable=variables_postres[contador])
+    
+    postres.grid(row=contador, column=0, sticky=W)
+    contador += 1
+
 #Evitar que la aplicacion se cierre
 aplicacion.mainloop()
