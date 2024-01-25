c3bc901f4267b709e3af0d309391cf97615183a1
Oscar Bocanegra <davidbocanegrac@gmail.com>
2024-01-25 18:29:11 -0500

    eliminando tarea

---
+++ python_total/projects/dia16/src/proyecto/base/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Tarea
+
+# Create your views here.
+class ListaPendientes(ListView):
+    model = Tarea
+    context_object_name = 'tareas'
+    
+class DetalleTarea(DetailView):
+    model = Tarea
+    context_object_name = 'tarea'
+    template_name = 'base/tarea.html'
+    
+class CrearTarea(CreateView):
+    model = Tarea
+    fields = '__all__'
+    success_url = reverse_lazy('tareas')
+    
+class EditarTarea(UpdateView):
+    model = Tarea
+    fields = '__all__'
+    success_url = reverse_lazy('tareas')
+    
+
+class EliminarTarea(DeleteView):
+    model = Tarea
+    context_object_name = "tarea"
+    success_url = reverse_lazy('tareas')
+    
