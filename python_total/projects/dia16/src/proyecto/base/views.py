19926abd1c5576e0653a9d289153b87323bde631
Oscar Bocanegra <davidbocanegrac@gmail.com>
2024-01-30 12:42:04 -0500

    informacion especifica

--- python_total/projects/dia16/src/proyecto/base/views.py
+++ python_total/projects/dia16/src/proyecto/base/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
@@ -20,6 +21,13 @@ class ListaPendientes(LoginRequiredMixin, ListView):
     model = Tarea
     context_object_name = 'tareas'
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tareas'] = context['tareas'].filter(usuario=self.request.user)
+        context['count'] = context['tareas'].filter(completo=False).count()
+        return context
+        
+    
 class DetalleTarea(LoginRequiredMixin, DetailView):
     model = Tarea
     context_object_name = 'tarea'
@@ -27,12 +35,16 @@ class DetalleTarea(LoginRequiredMixin, DetailView):
     
 class CrearTarea(LoginRequiredMixin, CreateView):
     model = Tarea
-    fields = '__all__'
+    fields = ['titulo', 'descripcion', 'completo']
     success_url = reverse_lazy('tareas')
+    
+    def form_valid(self, form):
+        form.instance.usuario = self.request.user
+        return super(CrearTarea, self).form_valid(form)
     
 class EditarTarea(LoginRequiredMixin, UpdateView):
     model = Tarea
-    fields = '__all__'
+    fields = ['titulo', 'descripcion', 'completo']
     success_url = reverse_lazy('tareas')
     
 
